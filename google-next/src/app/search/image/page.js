0468035f2e5bc9bcc66725db4ca7348bfc2173db
hunglee6 <sam86110306@gmail.com>
2024-05-31 23:23:36 -0500

    Add loading effect for web and image pages

--- google-next/src/app/search/image/page.js
+++ google-next/src/app/search/image/page.js
@@ -7,6 +7,8 @@ export default async function ImageSearchPage({ searchParams }) {
   const searchTerm = searchParams.searchTerm;
   const startIndex = searchParams.start || "1";
 
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
   // const response = await fetch(
   //   `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_API_KEY}&q=${searchTerm}&searchType=image&start=${startIndex}`
   // );
